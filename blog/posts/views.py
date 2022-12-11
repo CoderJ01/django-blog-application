63e052d08bb1fd1d2335acca3830d68e1154cfd0
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-10 21:33:50 -0800

    pass pk parameter (post id) to post function

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -6,5 +6,6 @@ def index(request):
     posts = Post.objects.all();
     return render(request, 'index.html', {'posts': posts}) # dynamically retrieve objects from posts
 
-def post(request):
-    return render(request, 'posts.html') 
+def post(request, pk): # id of the post will be passed to the function
+    posts = Post.objects.get(id=pk), # get id of post
+    return render(request, 'posts.html', {'posts': posts}) 
