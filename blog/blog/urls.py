ca4123a43ac3a908f7318301b4d9193f4fec8b4c
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-10 19:44:55 -0800

    configure blog/urls.py so that default page can be replaced

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# replace the default page on the site
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('posts.urls')) # once a user comes to the main page, the page should include
+                                    # its own url and posts.url
 ]
