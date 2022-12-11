d01cbc277d60ffa4b305530085293997e1654551
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-10 19:26:03 -0800

    set up first url pattern

---
+++ blog/posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path # default django function that allows urls to be configured
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index') # blank means the default url of the website
+                                        # when user comes to the website, the views.index function will be triggered
+]
