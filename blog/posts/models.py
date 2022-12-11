7f83568d09b12b0acf70623ee385c91628326a21
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-10 20:12:01 -0800

    fix error

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_Length=100) # CharField is a String field
-    body = models.CharField(max_Length=1_000_000)
+    title = models.CharField(max_length=100) # CharField is a String field
+    body = models.CharField(max_length=1_000_000)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
